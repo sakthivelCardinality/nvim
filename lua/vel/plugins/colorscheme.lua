0ca1df8003d6b62f2fba1c2b72c42f744ef6b70e
sakthivelCardinality <sakthivel.a@cardinality.ai>
2023-10-26 20:39:28 +0530

    :tada: :sparkles: updated plugins

--- lua/vel/plugins/colorscheme.lua
+++ lua/vel/plugins/colorscheme.lua
@@ -1,56 +1,50 @@
 return {
-	-- {
-	--    "bluz71/vim-nightfly-guicolors",
-	--    priority = 1000, -- make sure to load this before all the other start plugins
-	--    config = function()
-	--      -- load the colorscheme here
-	--      -- vim.cmd([[colorscheme nightfly]])
-	--    end,
-	--  },
 	{
 		"Mofiqul/dracula.nvim",
 		priority = 1000, -- make sure to load this before all the other start plugins
-		config = function()
-			require("dracula").setup({
-				-- customize dracula color palette
-				colors = {
-					bg = "#000000", -- 282A36
-					fg = "#F8F8F2",
-					selection = "#44475A",
-					comment = "#6272A4",
-					red = "#FF5555",
-					orange = "#FFB86C",
-					yellow = "#F1FA8C",
-					green = "#50fa7b",
-					purple = "#BD93F9",
-					cyan = "#8BE9FD",
-					pink = "#FF79C6",
-					bright_red = "#FF6E6E",
-					bright_green = "#69FF94",
-					bright_yellow = "#FFFFA5",
-					bright_blue = "#D6ACFF",
-					bright_magenta = "#FF92DF",
-					bright_cyan = "#A4FFFF",
-					bright_white = "#FFFFFF",
-					menu = "#000000", -- 21222C
-					visual = "#3E4452",
-					gutter_fg = "#4B5263",
-					nontext = "#3B4048",
-					white = "#ABB2BF",
-					black = "#191A21",
-				},
-				-- show the '~' characters after the end of buffers
-				show_end_of_buffer = true, -- default false
-				-- use transparent background
-				transparent_bg = true, -- default false
-				-- set custom lualine background color
-				lualine_bg_color = nil, -- default nil
-				-- set italic comment
-				italic_comment = true, -- default false
-				-- overrides the default highlights with table see `:h synIDattr`
-				overrides = {},
-			})
-			vim.cmd([[colorscheme dracula]])
+		lazy = false,
+		opts = {
+			-- customize dracula color palette
+			colors = {
+				bg = "#000000", -- 282A36
+				fg = "#F8F8F2",
+				selection = "#44475A",
+				comment = "#6272A4",
+				red = "#FF5555",
+				orange = "#FFB86C",
+				yellow = "#F1FA8C",
+				green = "#50fa7b",
+				purple = "#BD93F9",
+				cyan = "#8BE9FD",
+				pink = "#FF79C6",
+				bright_red = "#FF6E6E",
+				bright_green = "#69FF94",
+				bright_yellow = "#FFFFA5",
+				bright_blue = "#D6ACFF",
+				bright_magenta = "#FF92DF",
+				bright_cyan = "#A4FFFF",
+				bright_white = "#FFFFFF",
+				menu = "#000000", -- 21222C
+				visual = "#3E4452",
+				gutter_fg = "#4B5263",
+				nontext = "#3B4048",
+				white = "#ABB2BF",
+				black = "#191A21",
+			},
+			-- show the '~' characters after the end of buffers
+			show_end_of_buffer = true, -- default false
+			-- use transparent background
+			transparent_bg = true, -- default false
+			-- set custom lualine background color
+			lualine_bg_color = nil, -- default nil
+			-- set italic comment
+			italic_comment = true, -- default false
+			-- overrides the default highlights with table see `:h synIDattr`
+			overrides = {},
+		},
+		config = function(_, opts)
+			require("dracula").setup(opts)
+			vim.cmd.colorscheme("dracula")
 
 			-- Transparent telescope background
 			vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
