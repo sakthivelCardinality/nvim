0cecea0bbbcc59b910693a6faf652ef4b8fc3541
sakthivelCardinality <sakthivel.a@cardinality.ai>
2023-11-22 12:54:38 +0530

    :tada: :sparkles: updated plugins

--- lua/vel/plugins/git.lua
+++ lua/vel/plugins/git.lua
@@ -18,19 +18,36 @@ return {
 				local function map(mode, l, r, desc)
 					vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
 				end
-        -- stylua: ignore start
-        map("n", "]h", gs.next_hunk, "Next Hunk")
-        map("n", "[h", gs.prev_hunk, "Prev Hunk")
-        map({ "n", "v" }, "<leader>ghs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
-        map({ "n", "v" }, "<leader>ghr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
-        map("n", "<leader>ghS", gs.stage_buffer, "Stage Buffer")
-        map("n", "<leader>ghu", gs.undo_stage_hunk, "Undo Stage Hunk")
-        map("n", "<leader>ghR", gs.reset_buffer, "Reset Buffer")
-        map("n", "<leader>ghp", gs.preview_hunk, "Preview Hunk")
-        map("n", "<leader>ghb", function() gs.blame_line({ full = true }) end, "Blame Line")
-        map("n", "<leader>ghd", gs.diffthis, "Diff This")
-        map("n", "<leader>ghD", function() gs.diffthis("~") end, "Diff This ~")
-        map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
+
+				-- Action
+				map("n", "]h", gs.next_hunk, "Next Hunk")
+				map("n", "[h", gs.prev_hunk, "Prev Hunk")
+				map({ "n", "v" }, "<leader>hs", ":Gitsigns stage_hunk<CR>", "GitSigns Stage Hunk")
+				map({ "n", "v" }, "<leader>hr", ":Gitsigns reset_hunk<CR>", "GitSigns Reset Hunk")
+				map("n", "<leader>hS", gs.stage_buffer, "Stage Buffer")
+				map("n", "<leader>ha", gs.stage_hunk, "Stage Hunk")
+				map("n", "<leader>hu", gs.undo_stage_hunk, "Undo Stage Hunk")
+				map("n", "<leader>hR", gs.reset_buffer, "Reset Buffer")
+				map("n", "<leader>hp", gs.preview_hunk, "Preview Hunk")
+				map("n", "<leader>hb", function()
+					gs.blame_line({ full = true })
+				end, "Blame Line")
+				map("n", "<leader>hd", gs.diffthis, "Diff This")
+				map("n", "<leader>hD", function()
+					gs.diffthis("~")
+				end, "Diff This ~")
+
+				-- Toggle Git Ui
+				map("n", "<leader>ub", gs.toggle_current_line_blame, "Toggle Git Line Blame")
+				map("n", "<leader>ud", gs.toggle_deleted, "Toggle Git Delete")
+
+				-- Text Object
+				map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
+
+				local wk = require("which-key")
+				wk.register({
+					["<leader>h"] = { name = "+Git Hunk" },
+				})
 			end,
 		},
 	},
