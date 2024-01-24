8b0b1f8aadc48804338961c744b59e351b14d8fd
sakthivelCardinality <sakthivel.a@cardinality.ai>
2024-01-24 13:51:49 +0530

    :tada: :sparkles: updated plugins

--- lua/vel/plugins/telescope.lua
+++ lua/vel/plugins/telescope.lua
@@ -50,10 +50,10 @@ return {
 		},
 		opts = {
 			defaults = {
-				-- layout_strategy = "horizontal",
-				-- layout_config = { prompt_position = "top" },
-				-- sorting_strategy = "ascending",
-				-- winblend = 0,
+				layout_strategy = "horizontal",
+				layout_config = { prompt_position = "top" },
+				sorting_strategy = "ascending",
+				winblend = 0,
 				mappings = {
 					i = {
 						["<esc>"] = function(...)
