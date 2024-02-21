dd5b5f07f5149f21fcb2549534d4603f765e3352
sakthivelCardinality <sakthivel.a@cardinality.ai>
2024-02-21 20:39:03 +0530

    :tada: :sparkles: updated plugins

--- lua/vel/plugins/nvim-cmp.lua
+++ lua/vel/plugins/nvim-cmp.lua
@@ -51,6 +51,24 @@ return {
 					cmp.abort()
 					fallback()
 				end,
+				["<Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item()
+					elseif luasnip.expand_or_locally_jumpable() then
+						luasnip.expand_or_jump()
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+				["<S-Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item()
+					elseif luasnip.locally_jumpable(-1) then
+						luasnip.jump(-1)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
 			}),
 			-- sources for autocompletion
 			sources = cmp.config.sources({
