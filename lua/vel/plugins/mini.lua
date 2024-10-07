2ae4861f6f2c466b2bf2ad0966d5437f0aa364aa
sakthivelCardinality <sakthivel.a@cardinality.ai>
2024-10-07 09:50:51 +0530

    :tada: :sparkles: updated config

--- lua/vel/plugins/mini.lua
+++ lua/vel/plugins/mini.lua
@@ -1,7 +1,22 @@
 return {
 	{ "echasnovski/mini.statusline", event = "VeryLazy", opts = {} },
 	{ "echasnovski/mini.cursorword", event = "VeryLazy", opts = {} },
-	{ "echasnovski/mini.pairs", event = "VeryLazy", opts = {} },
+	{
+		"echasnovski/mini.pairs",
+		event = "VeryLazy",
+		opts = {
+			modes = { insert = true, command = true, terminal = false },
+			-- skip autopair when next character is one of these
+			skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+			-- skip autopair when the cursor is inside these treesitter nodes
+			skip_ts = { "string" },
+			-- skip autopair when next character is closing pair
+			-- and there are more closing pairs than opening pairs
+			skip_unbalanced = true,
+			-- better deal with markdown code blocks
+			markdown = true,
+		},
+	},
 	{
 		"echasnovski/mini.ai",
 		event = "VeryLazy",
@@ -17,13 +32,30 @@ return {
 					f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }, {}),
 					c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }, {}),
 					t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" },
-					g = function() -- Whole buffer, similar to `gg` and 'G' motion
-						local from = { line = 1, col = 1 }
-						local to = {
-							line = vim.fn.line("$"),
-							col = math.max(vim.fn.getline("$"):len(), 1),
-						}
-						return { from = from, to = to }
+					e = { -- Word with case
+						{
+							"%u[%l%d]+%f[^%l%d]",
+							"%f[%S][%l%d]+%f[^%l%d]",
+							"%f[%P][%l%d]+%f[^%l%d]",
+							"^[%l%d]+%f[^%l%d]",
+						},
+						"^().*()$",
+					},
+					g = function(ai_type) -- Whole buffer, similar to `gg` and 'G' motion
+						local start_line, end_line = 1, vim.fn.line("$")
+						if ai_type == "i" then
+							-- Skip first and last blank lines for `i` textobject
+							local first_nonblank, last_nonblank =
+								vim.fn.nextnonblank(start_line), vim.fn.prevnonblank(end_line)
+							-- Do nothing for buffer with all blanks
+							if first_nonblank == 0 or last_nonblank == 0 then
+								return { from = { line = start_line, col = 1 } }
+							end
+							start_line, end_line = first_nonblank, last_nonblank
+						end
+
+						local to_col = math.max(vim.fn.getline(end_line):len(), 1)
+						return { from = { line = start_line, col = 1 }, to = { line = end_line, col = to_col } }
 					end,
 				},
 			}
