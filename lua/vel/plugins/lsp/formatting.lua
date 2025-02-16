3df2e91faf67063feabb4b89d515cf7a32d62dfa
sakthivelCardinality <sakthivel.a@cardinality.ai>
2025-02-16 18:56:19 +0530

    :tada: :sparkles: updated config

--- lua/vel/plugins/lsp/formatting.lua
+++ lua/vel/plugins/lsp/formatting.lua
@@ -22,8 +22,8 @@ return {
 						timeout_ms = 5000,
 					})
 				end,
-				mode = { "n", "v" },
-				desc = "Format file or range (in visual mode)",
+				mode = { "n" },
+				desc = "Format file",
 			},
 			{
 				"<leader>uf",
@@ -62,12 +62,29 @@ return {
 				if vim.g.conform_autoformat == false then
 					return
 				end
-				return {
-					lsp_fallback = true,
-					async = false,
-					timeout_ms = 1000,
-				}
+				return { lsp_fallback = true, async = false, timeout_ms = 1000 }
 			end,
 		})
+
+		vim.api.nvim_create_user_command("ConformRangeFormat", function(args)
+			local range = nil
+			if args.count ~= -1 then
+				local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+				range = {
+					start = { args.line1, 0 },
+					["end"] = { args.line2, end_line:len() },
+				}
+			end
+			require("conform").format({ async = true, lsp_format = "fallback", range = range }, function(err)
+				if not err then
+					local mode = vim.api.nvim_get_mode().mode
+					if vim.startswith(string.lower(mode), "v") then
+						vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "n", true)
+					end
+				end
+			end)
+		end, { range = true })
+
+		vim.keymap.set({ "v" }, "<leader>cf", "<cmd>ConformRangeFormat<cr>", { desc = "Range Format" })
 	end,
 }
