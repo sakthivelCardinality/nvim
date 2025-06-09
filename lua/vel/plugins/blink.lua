51c025b27ea9f67efe12af0f6e1251622d2daad8
sakthivelCardinality <sakthivel.a@cardinality.ai>
2025-06-09 17:17:58 +0530

    🎉 ✨ updated config

--- lua/vel/plugins/blink.lua
+++ lua/vel/plugins/blink.lua
@@ -43,7 +43,12 @@ return {
 
 		-- (Default) Only show the documentation popup when manually triggered
 		completion = {
-			documentation = { auto_show = true },
+			menu = {
+				draw = {
+					treesitter = { "lsp" },
+				},
+			},
+			documentation = { auto_show = true, auto_show_delay_ms = 200 },
 			ghost_text = { enabled = true },
 		},
 
